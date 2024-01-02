88bcf57dd43e8cde88cb5d6090ba4ae37c0673ad
faridabderrahmani <abderrahmani@yandex.ru>
2024-01-02 12:54:09 +0100

    First Try

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -7,18 +7,20 @@ package school.mjc.stage0.loops.task4;
 public class NumberToBreakOn {
     public static void main(String[] args) {
 
-
+printNumbersUntilWithBreakOn(15, 20);
     }
-    public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+    public static void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+        String textLine = "iterating till the end ";
         for(int i = 1; i <= numberToGoUntil; i++){
-            if(numberToGoUntil < toBreakWith){
-                System.out.println("iterating till the end");
-                System.out.println(i);
-            }else {
+            if(numberToGoUntil < toBreakWith) {
+                textLine += "\n"+i;
+            }
+            else {
                 if (i > toBreakWith){
                     break;
                 }System.out.println(i);
+
             }
-        }
+        }System.out.println(textLine);
     }
 }
